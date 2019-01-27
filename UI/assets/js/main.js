c5cb498aba171c6d31f86d1cd233ac819ea4a93e
Ibukunoluwa Adeeko <ibukunadeeko@gmail.com>
2019-01-28 00:18:55 +0100

    [feature #163478500] Created Admin Pages
    I created admin pages that had the following:
    A page to create and edit political parties
    A page to create political offices
    A page to vies the election results
    A page to manage candidate submissions

---
+++ UI/assets/js/main.js
@@ -0,0 +1,31 @@
+const menuToggle = document.querySelector('.menu-toggle'); // Menu Toggle
+const navBar = document.querySelector('nav'); // Menu Toggle
+const sideNav = document.querySelector('.sidenav'); // ADMIN BAR TOGGLE
+const adminToggle = document.querySelector('.admin-toggle'); // ADMIN BAR TOGGLE
+const closebtn = document.querySelector('.closebtn'); // ADMIN BAR TOGGLE
+
+function openNav() {
+  sideNav.style.width = '200px';
+}
+
+function closeNav() {
+  sideNav.style.width = '0';
+}
+
+// MENU TOGGLE
+if (menuToggle && navBar) {
+  menuToggle.addEventListener('click', () => {
+    closeNav();
+    navBar.classList.toggle('active');
+  });
+}
+
+// ADMIN BAR TOGGLE
+if (sideNav && adminToggle && closebtn) {
+  adminToggle.addEventListener('click', () => {
+    openNav();
+  });
+  closebtn.addEventListener('click', () => {
+    closeNav();
+  });
+}
