8c78bd6472407352d0fa786d67a4c01406ad792b
Ibukunoluwa Adeeko <ibukunadeeko@gmail.com>
2019-02-06 06:55:38 +0100

    Added and integrated .env to the project

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,13 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new Pool({
-  user: 'ibukun',
-  database: 'politico',
-  password: 'password',
-  port: 5432,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
   max: 40, // max number of connection can be open to database
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 });
