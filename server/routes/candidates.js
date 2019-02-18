2d9f20906c084db9adf6e0a04a42d6852b71c0dd
Ibukunoluwa Adeeko <ibukunadeeko@gmail.com>
2019-02-18 12:43:07 +0100

    Added cors and made changes to token

--- server/routes/candidates.js
+++ server/routes/candidates.js
@@ -1,11 +1,12 @@
 import Express from 'express';
 import candidates from '../controllers/candidates';
 import validate from '../middleware/validateCandidatesInput';
+import tokenValidator from '../middleware/verifyToken';
 
 const router = Express.Router();
 
-router.post('/office/:userid/register', validate.id, candidates.register);
-router.post('/office/:officeid/result', validate.result, candidates.result);
-router.post('/vote', validate.vote, candidates.vote);
+router.post('/office/:userid/register', tokenValidator.verifyToken, validate.id, candidates.register);
+router.post('/office/:officeid/result', tokenValidator.verifyToken, validate.result, candidates.result);
+router.post('/vote', tokenValidator.verifyToken, validate.vote, candidates.vote);
 
 export default router;
